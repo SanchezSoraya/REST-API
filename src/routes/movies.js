28cda64fdeb9b5d2ab373d12381b54587a9feacd
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-04-22 09:58:22 -0500

    peticiones: Get, Post, Put, Delete

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
+const _ = require('underscore');
 
 const movies = require('../sample.json');
 
@@ -21,8 +22,32 @@ router.post('/', (req, res) => {
     }
 
 });
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    if (id && title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({ error: 'Hubo un error' });
+    }
+
+});
+
 router.delete('/:id', (req, res) => {
-    console.log(req.params);
+    const { id } = req.params;
+    _.each(movies, (movie, i) => {
+        if (movie.id == id) {
+            movies.splice(i, 1);
+        }
+    });
     res.send('deleted');
 });
 
